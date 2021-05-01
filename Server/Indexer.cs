1714e839a29f2f86d33d60d05c1eb5f222edeaed
Dekardt <sasha32083@gmail.com>
2021-05-01 15:43:57 +0300

    Add single and multi thread creation of index

--- Server/Indexer.cs
+++ Server/Indexer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -15,12 +14,12 @@ namespace Server
         private static string[] ExcludedWords = File.ReadAllText("..//..//..//stop_words.txt").Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
         // amount of threads used for building index
-        const int ThreadsAmount = 1;
+        private const int ThreadsAmount = 2;
 
         // datasets
         FileInfo[] filesList;
 
-        private ConcurrentDictionary<string, List<string>> invertedIndex;
+        public ConcurrentDictionary<string, List<string>> invertedIndex = new ConcurrentDictionary<string, List<string>>();
 
 
         public Indexer(string dirPath)
@@ -30,8 +29,7 @@ namespace Server
         }
 
 
-        // pre
-        private static string[] LexemsPreproccessing(string sourceFileText)
+        private static string[] LexemsPreprocessing(string sourceFileText)
         {
             string fileText = Regex.Replace(sourceFileText, "< *br */ *>", "");
             fileText = Regex.Replace(fileText, "[0-9]+", "");
@@ -42,13 +40,44 @@ namespace Server
         }
 
 
-        public void CreateInvertIndex(int startFile, int endFile)
+        private void ParseBlock(int startFileIndex, int endFileIndex)
         {
-            for (int i = startFile; i < endFile; i++)
+            for(int i=startFileIndex; i<endFileIndex; i++)
             {
-                Console.WriteLine(filesList[i].FullName);
+                FileInfo curFile = this.filesList[i];
+                string[] lexemLists = LexemsPreprocessing(File.ReadAllText(curFile.FullName));
+
+                foreach(string lexem in lexemLists)
+                {
+                    if (!ExcludedWords.Contains(lexem))
+                    {
+                        this.invertedIndex.AddOrUpdate(lexem, new List<string> { curFile.Name }, (k, v) => v.Append(curFile.Name).ToList());
+                    }
+                }
             }
         }
 
+
+        public void CreateIndex()
+        {
+            if(ThreadsAmount == 1)
+            {
+                ParseBlock(0, this.filesList.Length);
+            }
+            else
+            {
+                float filePerThread = this.filesList.Length / (float)ThreadsAmount;
+
+                Task[] tasks = new Task[ThreadsAmount];
+
+                for(int iter=0; iter<ThreadsAmount; iter++)
+                {
+                    int startFile = (int)(filePerThread * iter);
+                    int endFile = (int)(filePerThread * (iter + 1));
+                    tasks[iter] = Task.Run(() => ParseBlock(startFile, endFile));
+                }
+                Task.WaitAll(tasks);
+            }
+        }
     }
 }
